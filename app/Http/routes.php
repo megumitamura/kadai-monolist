4b87585d801803e52f17eb6f14590701bbf27239
megumi.tamura <ec2-user@ip-172-31-34-87.us-east-2.compute.internal>
2018-04-30 14:10:21 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,7 @@ Route::get('/', 'WelcomeController@index');
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
